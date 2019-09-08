caf13b70d6972764da9e9f7a3cac180cebef4042
ashrakrahman <ashrakrahman@gmail.com>
2019-09-08 23:26:06 +0600

    redux structure tree and debug tool intregation

--- src/index.js
+++ src/index.js
@@ -3,9 +3,43 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import * as serviceWorker from "./serviceWorker";
 import "bootstrap/dist/css/bootstrap.css";
-import Calculator from "./components/calculator";
+import App from "./App";
+import { Provider } from "react-redux";
+import calculator from "./reducers/calculatorReducer";
+import { createStore } from "redux";
 
-ReactDOM.render(<Calculator />, document.getElementById("root"));
+const initialState = {
+  btnList: [
+    { id: 1, name: "btn1", display: "1" },
+    { id: 2, name: "btn2", display: "2" },
+    { id: 3, name: "btn3", display: "3" },
+    { id: 4, name: "btn4", display: "+" },
+    { id: 5, name: "btn5", display: "4" },
+    { id: 6, name: "btn6", display: "5" },
+    { id: 7, name: "btn7", display: "6" },
+    { id: 8, name: "btn8", display: "-" },
+    { id: 9, name: "btn9", display: "7" },
+    { id: 10, name: "btn10", display: "8" },
+    { id: 11, name: "btn11", display: "9" },
+    { id: 12, name: "btn12", display: "*" }
+  ],
+  displayId: ""
+};
+
+const store = createStore(
+  calculator,
+  initialState,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+const rootElement = document.getElementById("root");
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  rootElement
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ src/reducers/calculatorReducer.js
@@ -0,0 +1,8 @@
+export default function calculator(state = [], action) {
+  switch (action.type) {
+    case "ADD_TODO":
+      return state;
+    default:
+      return state;
+  }
+}
